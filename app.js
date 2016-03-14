9f3c60d9673b5852c00f69da70833b410999f3d5
lfulbel <lfulbel@iastate.edu>
2016-03-14 01:38:03 -0500

    getting started on the d3 library

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ api.store.put("mySecretKey", 6000);
 
 var results = api.boa.run("my-boa-script.boa");
 
-var results = api.boa.exec("# How many committers are in each project?"
-"p: Project = input;"
-"counts: output sum[string] of int;"
-
-"committers: map[string] of bool;"
-
-"visit(p, visitor {"
-"	before node: Revision ->"
-"		if (!haskey(committers, node.committer.username)) {"
-"			committers[node.committer.username] = true;"
-"			counts[p.id] << 1;"
+var results = api.boa.exec("p: Project = input;"+
+"counts: output sum[string] of int;"+
+"committers: map[string] of bool;"+
+"visit(p, visitor {"+
+"	before node: Revision ->"+
+"		if (!haskey(committers, node.committer.username)) {"+
+"			committers[node.committer.username] = true;"+
+"			counts[p.id] << 1;"+
 "		}");
