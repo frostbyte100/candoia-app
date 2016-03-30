1eb8a636c98c7679a64745d18030bb0fae95e93e
lfulbel <lfulbel@iastate.edu>
2016-03-30 11:01:40 -0500

    added these libraries for help

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-console.log("Hello World");
+console.log("Testing if this is even showing");
 
 api.store.put("mySecretKey", 6000);
 
 var results = api.boa.run("my-boa-script.boa");
 
-var results = api.boa.exec("p: Project = input;"+
-"counts: output sum[string] of int;"+
-"committers: map[string] of bool;"+
-"visit(p, visitor {"+
-"	before node: Revision ->"+
-"		if (!haskey(committers, node.committer.username)) {"+
-"			committers[node.committer.username] = true;"+
-"			counts[p.id] << 1;"+
-"		}");
+// var results = api.boa.exec("p: Project = input;"+
+// "counts: output sum[string] of int;"+
+// "committers: map[string] of bool;"+
+// "visit(p, visitor {"+
+// "	before node: Revision ->"+
+// "		if (!haskey(committers, node.committer.username)) {"+
+// "			committers[node.committer.username] = true;"+
+// "			counts[p.id] << 1;"+
+// "		}");
+
+for (var x in results)
+{
+  // for (var y in x)
+  // {
+    console.log(x);
+  // }
+};
