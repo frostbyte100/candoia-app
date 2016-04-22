55d18f96b37cec4645c0c4914f656f8ba9f03bba
lfulbel <lfulbel@iastate.edu>
2016-04-21 19:47:38 -0500

    made more design changes

--- app.js
+++ app.js
@@ -1,33 +1,80 @@
 'use strict';
 $(window).load(function() {
-    console.log("True start!");
-
-    api.store.put("mySecretKey", 6000);
-
-    let json = api.boa.run("right.boa");
-
-
-    console.log(json);
 
     var countRev = 0;
-    var s = "";
-    for(let time in json['names'])
+
+
+    window.getData = function(){
+      api.store.put("mySecretKey", 6000);
+      console.log("Getting Boa");
+      let json = api.boa.run("right.boa");
+      console.log("Done fetching Boa");
+      return json;
+    }
+    window.timeConverter = function(UNIX_timestamp){
+      var a = new Date(UNIX_timestamp/1000);
+      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      var sec = a.getSeconds();
+      var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+      return time;
+    }
+    window.showAnimation = function()
     {
-      countRev++;
-      console.log(countRev);
-      var d = new Date(time);
-      var q = d.getTime()*1000;
-      s += "<h2>" + time +"</h2>"
-      for(let name in json['names'][time])
-      {
-        s+="<p>"+json['names'][time][name]+"</p>";
-      }
-      s+="<br>"
-      console.log(s);
+
+    }
+    window.convertToUnix = function(x)
+    {
+      var m = parseInt(x.substring(0,2));
+      var y = parseInt(x.substring(3,x.length));]
+      var k = new Date(y, m);
+      return k.getTime()/1000;
     }
 
+    window.showResults = function()
+    {
+      console.log("This is starting!");
+      var star = parseInt(convertToUnix( document.getElementById("start").value ))*10000;
+      var end = parseInt(convertToUnix(document.getElementById("end").value))*10000;
 
-    $("#display").html("<div>"+s+"</div>");
+      console.log("Start: " +  star);
+      console.log("End: " + end);
+      if(star >= end){
+        $("#display").html("<div>"+"ERROR: RANGE DOES NOT EXIST"+"</div>");
+      }
+      // if(star == end){
+      //   end = timeConverter(star);
+      // }
+      var json = getData();
 
-    console.log("End of everything!");
+      var s = "";
+      console.log("Starting appending");
+      for(let time in json['names'])
+      {
+
+          countRev++;
+          console.log("This is the time: " + parseInt(time));
+          console.log("This is the start: " + star);
+          if( star <= parseInt(time) && parseInt(time) <=end)
+          {
+            s += "<h2>" + timeConverter( parseInt(time) ) +"</h2>"
+            for(let name in json['names'][time])
+            {
+              s+="<p>"+json['names'][time][name]+"</p>";
+              console.log(json['names'][time][name]);
+            }
+            s+="<br>";
+
+          }
+
+          console.log(s);
+
+      }
+      console.log("Attempting to use jquery");
+      $("#display").html("<div>"+s+"</div>");
+    }
 });
